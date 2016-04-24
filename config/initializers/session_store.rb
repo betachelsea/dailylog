c02aa7340c2ed8090228972c007986648d2c2cd4
beta_chelsea <beta.chelsea@gmail.com>
2016-04-24 19:56:56 +0900

    Rename project name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_easytask_session'
+Rails.application.config.session_store :cookie_store, key: '_dailyreport_session'
