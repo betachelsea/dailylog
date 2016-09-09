14cbddb0534e54b1881d489da6e82a08e19ea25b
beta_chelsea <beta.chelsea@gmail.com>
2016-09-09 16:12:08 +0900

    Capistrano公式のUsageに従う

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,12 @@
-# config valid only for current version of Capistrano
-lock '3.6.1'
-
-set :application, 'dailylog'
-set :repo_url, 'git@github.com:betachelsea/dailylog.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'master'
-
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/app/dailylog'
-
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'bundle', 'public/system', 'public/assets'
-
-# Default value for default_env is {}
-set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
-
-set :log_level, :debug
-set :bundle_binstubs, nil
-
-namespace :deploy do
-  after :finishing, 'deploy:cleanup'
-end
+# ref. https://github.com/capistrano/rails
+set :rails_env, 'staging'
+set :migration_role, :db
+set :migration_servers, -> { primary(fetch(:migration_role)) }
+set :conditionally_migrate, true
+set :assets_roles, [:web, :app]
+set :assets_prefix, 'prepackaged-assets'
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+set :keep_assets, 2
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :migration_role, :app
