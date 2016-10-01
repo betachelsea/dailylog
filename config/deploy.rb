2fde89a266dd8dc6d208b8de2a4978ea4f5b3fd8
beta_chelsea <beta.chelsea@gmail.com>
2016-10-01 18:13:11 +0900

    config/secrets.ymlにシンボリックリンクを貼る
    サーバ上では、current/config/secrets.ymlのファイルは
    シンボリックリンク化する。
    実際のファイルは shared/config/secrets.ymlに用意する。

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :bundle_binstubs, nil
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
 set :default_env, { path: "usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
 set :keep_releases, 5
+set :linked_files, %w{ config/secrets.yml }
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
